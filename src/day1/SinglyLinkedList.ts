a50ebbb9d111ce2648dd20ab48149a2446a1dbf3
Puneet <macbook@Puneets-MacBook-Pro.local>
2023-07-08 17:44:08 -0400

    Making al inked list

--- src/day1/SinglyLinkedList.ts
+++ src/day1/SinglyLinkedList.ts
@@ -1,142 +1,93 @@
-export class Node<T> {
-	public next: Node<T> | null = null;
-	public prev: Node<T> | null = null;
-
-	constructor(public value: T | null) {
-		this.value = null
-		this.prev = null
-		this.next = null
-	}
+type Node<T> = {
+	value: T,
+	next?: Node<T>
+	prev?: Node<T>
 }
 export default class SinglyLinkedList<T> {
     public length: number
-		public head: Node<T> | null
-		public tail: Node<T> | null
+		public head?: Node<T>
+		public tail?: Node<T>
 		
     constructor() {
-			this.head = null
+			this.head = undefined
+			this.tail = undefined
 			this.length = 0
     }
 
     prepend(item: T): void {
-			const newNode = new Node(item)
-			if (!this.head || this.tail) {
-				this.head = newNode
-				this.tail = newNode
-			} else {
-				//get the current head
-				const currentHead = this.head
-				//point the current heads previous pointer towards the new node
-				currentHead.prev = newNode
-				//point the new nodes next point to the current
-				newNode.next = currentHead
-				this.length++
+			const node = {value: item} as Node<T>
+			if(!this.head) {
+				this.head = this.tail = node
+				return
 			}
+			const currHead = this.head
+			node.next = currHead
+			currHead.prev = node
+			this.head = node
 		}
     insertAt(item: T, idx: number): void {
-			const newNode = new Node(item)
+			const node = {value:item} as Node<T>
 			let targetNode = this.head
-			for (let i = 0; i < idx; i++) {
-				if(targetNode!.next) {
-					targetNode = targetNode!.next
-				}
+
+			if(!this.head) {
+				this.head = this.tail = node
+				return
 			}
-			let preNode = targetNode?.prev
-			preNode!.next = newNode
-			targetNode!.prev = newNode
-			this.length++
+			for (let i = 0; i < idx && targetNode; i++) {
+				targetNode = targetNode.next
+			}
+			const preNode = targetNode?.prev
+			const postNode = targetNode?.next
+
+			if(preNode){
+				preNode.next = node
+				node.prev = preNode
+			}
+
+			if(postNode){
+				postNode.prev = node
+				node.next = postNode
+			}
 		}
     append(item: T): void {
-			const newNode = new Node(item)
-			if(!this.head || !this.tail) {
-				this.head = newNode
-				this.tail = newNode
-			} else {
-				//get the current tail
-				const currentTail = this.tail
-				//point the current tails next towards the new node
-				currentTail.next = newNode
-				//point the new nodes previous to the current tail
-				newNode.prev = currentTail
-				this.length++
+			const node = {value: item} as Node<T>
+			if(!this.tail){
+				this.tail = this.head = node
 			}
+			this.tail.next = node
+			node.prev = this.tail
+			this.tail = node
 		}
     remove(item: T): T | undefined {
-			let targetNode = this.head
-			//Empty linked list
-			if(this.length === 0){
+			let curr = this.head
+			let target
+			if(!this.head) {
 				return undefined
 			}
-			//Only one node
-			if(this.length === 1 && targetNode){
-				targetNode.value = null
-				targetNode.prev = null
-				targetNode.next = null
-				return undefined
-			}
-			//Multiple nodes
-			for (let i = 0; i < this.length; i++) {
-				targetNode = targetNode!.next
-				if(targetNode?.value === item) {
-					targetNode = targetNode!.next
+
+			for (let i = 0; i < this.length && curr; i++) {
+				curr = curr.next
+				if(curr?.value === item){
+					target = curr
 				}
+				break
 			}
-			let preNode
-			let postNode
-			if(targetNode?.prev || targetNode?.next){
-				preNode = targetNode.prev
-				postNode = targetNode.next
-			}
-			if(preNode && postNode) {
-				preNode.next = postNode
-				postNode.prev = preNode
-			}
-			return undefined
+			const preNode = target?.prev
+			const postNode = target?.next
+
+			return target?.value
 		}
     get(idx: number): T | undefined | null {
-			let current = this.head
-			if(this.length === 0){
+			let curr = this.head
+
+			if(!this.head) {
 				return undefined
 			}
-			if(this.length === 1) {
-				return current?.value
+			for (let i = 0; i < idx && curr; i++) {
+				curr = curr.next
 			}
-			for (let i = 0; i < idx; i++) {
-				if(current!.next) {
-					current = current!.next
-				}
-			}
-			return current?.value
+			return curr?.value
 		}
     removeAt(idx: number): T | undefined {
-			let targetNode = this.head
-			//Empty linked list
-			if(this.length === 0){
-				return undefined
-			}
-			//Only one node
-			if(this.length === 1 && targetNode){
-				targetNode.value = null
-				targetNode.prev = null
-				targetNode.next = null
-				return undefined
-			}
-			//Multiple nodes
-			for (let i = 0; i < idx; i++) {
-				if(targetNode!.next) {
-					targetNode = targetNode!.next
-				}
-			}
-			let preNode
-			let postNode
-			if(targetNode?.prev || targetNode?.next){
-				preNode = targetNode.prev
-				postNode = targetNode.next
-			}
-			if(preNode && postNode) {
-				preNode.next = postNode
-				postNode.prev = preNode
-			}
-			return undefined
 		}
 }
