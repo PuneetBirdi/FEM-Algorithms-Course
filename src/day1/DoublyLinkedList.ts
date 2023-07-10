5412bbdf3b1ce3d8a8f2ea353665ec6cd50c24c3
Puneet <macbook@Puneets-MacBook-Pro.local>
2023-07-10 16:15:59 -0400

    WHY WONT MY FOR LOOP TRIGGER

--- src/day1/DoublyLinkedList.ts
+++ src/day1/DoublyLinkedList.ts
@@ -16,34 +16,42 @@ export default class DoublyLinkedList<T> {
 
     prepend(item: T): void {
 			const node = { value: item } as Node<T>
-			if(!this.head){
-				this.head = node
-				this.tail = node
-				this.length++
-			}
-			const currHead = this.head
-			node.next = currHead
-			currHead.prev = node
-		}
-    insertAt(item: T, idx: number): void {
-			const node = { value: item } as Node<T>
+
 			if(!this.head || !this.tail){
 				this.head = this.tail = node
 				this.length++
 				return
 			}
+			const currentHead = this.head
+			node.next = currentHead
+			currentHead.prev = node
+			this.head = node
+			this.length++
+		}
+    insertAt(item: T, idx: number): void {
+			const node = {value:item} as Node<T>
+			
+			//Empty list
+			if(!this.head || !this.tail){
+				this.head = this.tail = node
+				this.length++
+				return
+			}
+
+			//Insert at start
 			if(idx === 0){
-				const currHead = this.head
-				currHead.prev = node
-				node.next = currHead
+				const currentHead = this.head
+				currentHead.prev = node
+				node.next = currentHead
 				this.head = node
 				this.length++
 				return
 			}
+			//Insert at end
 			if(idx === this.length - 1){
-				const currTail = this.tail
-				currTail.next = node
-				node.prev = currTail
+				const currentTail = this.tail
+				currentTail.next = node
+				node.prev = currentTail
 				this.tail = node
 				this.length++
 				return
@@ -58,117 +66,105 @@ export default class DoublyLinkedList<T> {
 			const prevNode = targetNode.prev
 			if(prevNode){
 				prevNode.next = node
+				node.prev = prevNode
+				node.next = targetNode
+				targetNode.prev = node
+				this.length++
+				return
 			}
-			targetNode.prev = node
-			node.prev = prevNode
-			node.next = targetNode
-			this.length++
 			return
 		}
     append(item: T): void {
 			const node = { value: item } as Node<T>
-			if(!this.tail){
-				this.tail = node
-				length++
-					return
+
+			if(!this.head || !this.tail){
+				this.head = this.tail = node
+				this.length++
+				return
 			}
+
 			const currentTail = this.tail
-			currentTail.next = node
 			node.prev = currentTail
+			currentTail.next = node
 			this.tail = node
+			this.length++
 		}
     remove(item: T): T | undefined {
-			if(!this.head || !this.tail){
+			return undefined
+		}
+    get(idx: number): T | undefined {
+			if(!this.head || !this.tail || (idx > this.length - 1)){
 				return undefined
 			}
-			if(this.head.value === item){
-				const value = this.head.value
-				this.head = this.head.next
-				this.length--
-				return value
+			if(idx === 0){
+				return this.head.value
 			}
-			if(this.tail.value === item){
-				const value = this.tail.value
-				this.tail = this.tail.prev
-				this.length--
-				return value
+			if(idx === this.length - 1){
+				return this.tail.value
 			}
-			let curr = this.head
-			for(let i = 0; i < this.length && curr.next; i++){
-				if(curr.value === item){
-					break
-				} else {
-					curr = curr.next
+
+			let targetNode = this.head
+			for(let i = 0; i < idx - 1; i++){
+					if(targetNode.next){
+						targetNode = targetNode.next
+					}
+			}
+			return targetNode.value
+		}
+    removeAt(idx: number): T | undefined {
+			if(!this.head || !this.tail || (idx > this.length - 1)){
+				return undefined
+			}
+
+			//Remove Head
+			if(idx === 0){
+				const currentHead = this.head
+				if(!currentHead.next){
+					this.length--
+					return currentHead.value
+				}
+				const newHead = currentHead.next
+				newHead.prev = undefined
+				currentHead.next = undefined
+				this.head = newHead
+				this.length--
+				return currentHead.value
+			}
+
+			//Remove tail
+			if(idx === this.length - 1){
+				const currentTail = this.tail
+				if(!currentTail.prev){
+					this.length--
+					return currentTail.value
+				}
+				const newTail = currentTail.prev
+				newTail.prev = undefined
+				this.tail = newTail
+				this.length--
+				return currentTail.value
+			}
+
+			//Remove inbetweens
+			let targetNode = this.head
+			for(let i = 0; i < idx - 1 && targetNode; i++){
+				console.log('WHHHHHHHHHHHHHHHHHHHHHHHHHYYYYYYYYYYYYYYY')
+				if(targetNode.next){
+					targetNode = targetNode.next
 				}
 			}
-			const prevNode = curr.prev
-			const nextNode = curr.next
+			const prevNode = targetNode.prev
+			const nextNode = targetNode.next
+			console.log(targetNode)
 			if(prevNode){
 				prevNode.next = nextNode
 			}
 			if(nextNode){
 				nextNode.prev = prevNode
 			}
-			curr.prev = undefined
-			curr.next = undefined
+			targetNode.next = undefined
+			targetNode.prev = undefined
 			this.length--
-			return curr.value
-		}
-    get(idx: number): T | undefined {
-			if(!this.head || !this.tail){
-				return undefined
-			}
-
-			if(idx === 0){
-				return this.head.value
-			}
-
-			if(idx === this.length - 1){
-				return this.tail.value
-			}
-
-			let curr = this.head
-			for(let i = 0; i < idx && curr.next; i++) {
-				curr = curr.next
-			}
-			if(!curr) {
-				return undefined
-			}
-			return curr?.value
-		}
-    removeAt(idx: number): T | undefined {
-			if(!this.head || !this.tail){
-				return undefined
-			}
-			if(idx === 0){
-				const currNode = this.head
-				const nextNode = this.head.next
-				if(nextNode){
-					nextNode.prev = undefined
-				}
-				this.head = this.head.next
-				this.length--
-				return currNode.value
-			}
-
-			let currNode = this.head
-			for(let i = 0; i < idx - 1 && currNode; i++){
-				if(currNode.next){
-					currNode = currNode.next
-				} else{
-					return undefined
-				}
-			}
-			const targetNode = currNode.next
-			let nextNode
-			if(targetNode?.next){
-				nextNode = targetNode.next
-				currNode.next = nextNode
-				targetNode.next = undefined
-				this.length--
-				return targetNode.value
-			}else {
-				return undefined
-			}
+			return targetNode.value
 		}
 }
