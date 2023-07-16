49e1589516264946afd1b0719c010dc965d26910
Puneet <macbook@Puneets-MacBook-Pro.local>
2023-07-16 09:57:02 -0400

    Maze solver

--- src/day1/ArrayList.ts
+++ src/day1/ArrayList.ts
@@ -1,14 +1,23 @@
 export default class ArrayList<T> {
     public length: number;
-
+		public capacity: number;
+		public values: Array<number>
     
 
-    constructor() {
+    constructor(length: number, capacity: number, values: Array<T> ) {
+			this.length = 0 || length
+			this.capacity = 3 || capacity
+			this.values = new Array().fill(null, this.capacity) || values
     }
 
     prepend(item: T): void {
-
-}
+			//If this is full then make a new one and reassign
+			if(this.length === this.capacity){
+				const newValues = [item, ...this.values]
+				const newArrayList = new ArrayList(this.length + 1, this.capacity + 1, newValues)
+				this = newArrayList
+			}
+		}
     insertAt(item: T, idx: number): void {
 
 }
